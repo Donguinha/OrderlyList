a4a695bfe8eab36096df641edf95e1866be095b5
Guilherme Rodrigues <oliveirack1@gmail.com>
2022-11-04 14:29:10 -0300

    Alterações feitas

--- OrderlyList/Employees.cs
+++ OrderlyList/Employees.cs
@@ -1,38 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OrderlyList {
-    internal class Employees {
+    internal class Employee {
+        public int Quantity { get; set; }
         public int Id { get; set; }
         public string Nome { get; set; }
-        public int Ordem { get; set; }
 
-        public Employee(int Ordem, int Id, string Nome) {
-            this.Ordem = Ordem;
+        public Employee(int Quantity, int Id, string Nome) {
+            this.Quantity = Quantity;
             this.Id = Id;
             this.Nome = Nome;
         }
-        List<Employee> employees = new List<Employee>();
-        public void ReceberId(int emp, int idrecebido, string nomerecebido, int Quantos) {
-            Console.Write("how many employees does the company have: ");
-            emp = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < Quantos; i++) {
-                Console.Write("-----------------------------\n#{0}-Employee ID: ", (i + 1));
-                idrecebido = int.Parse(Console.ReadLine());
-                Console.Write("#{0}-Employee name: ", (i + 1));
-                nomerecebido = Console.ReadLine();
-                employees.Add(new Employee(i, idrecebido, nomerecebido));
-            }
-        }
+        
 
 
         override public string ToString() {
 
-            return $"#{Ordem + 1}- ID: {Id}, Name: {Nome}";
+            return $"#{Quantity + 1}- ID: {Id}, Name: {Nome}";
         }
     }
 }
--- OrderlyList/Program.cs
+++ OrderlyList/Program.cs
@@ -1,23 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
 
 namespace OrderlyList {
+
+
     class Program {
         static void Main(string[] args) {
 
-            List<Employee> employees = new List<Employee>();
-            employees.RecberId()
+            List<Employee> employees = DataReceive();
+
+            var employeesorder = Ordenar(employees);
+            Printar(employeesorder);
 
 
+        }
+        public static List<Employee> DataReceive() {
 
-            Console.WriteLine();
-            for (int i = 0; i < 1000; i++) {
-                foreach (Employee cada in list) {
-                    if (cada.Id == i) {
-                        Console.WriteLine(cada);
+            Console.Write("how many employees does the company have: ");
+            int Quantity = int.Parse(Console.ReadLine());
+
+            var list = new List<Employee>();
+
+
+            for (int i = 0; i < Quantity; i++) {
+
+                Console.Write("-----------------------------\n#{0}-Employee ID: ", (i + 1));
+                int IdReceived = int.Parse(Console.ReadLine());
+                Console.Write("#{0}-Employee name: ", (i + 1));
+                string NameReceived = Console.ReadLine();
+                list.Add(new Employee(Quantity, IdReceived, NameReceived));
+
+            }
+            return list;
+        }
+        public static List<Employee> Ordenar(List<Employee> employees) {
+
+            var ordenados = new int[employees.Count];
+
+
+            foreach (Employee cada in employees) {
+                for (int i = 0; i < ordenados.Length; i++) {
+                    if (cada.Id  <   ordenados[i]  || ordenados[i] == 0 ) {
+
+
+                        if (ordenados[i] != 0) {
+                            ordenados[i + 1] = ordenados[i];
+                        }
+
+                        ordenados[i] = cada.Id;
+                        break;
                     }
                 }
             }
+            var listanova = new List<Employee>();
+            foreach (int Id in ordenados) {
+                listanova.Add(employees.First(x => x.Id == Id));
+            }
+
+            return listanova;
+
+        }
+        public static void Printar(List<Employee> employees) {
+
+            foreach (Employee cada in employees) {
+
+                Console.WriteLine(cada);
+
+            }
+
+
         }
     }
 }
