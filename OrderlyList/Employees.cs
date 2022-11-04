e112de26a4787f3ab058b8f3b94c8ec3f4a30cf6
Guilherme Rodrigues <oliveirack1@gmail.com>
2022-11-04 18:24:20 -0300

    Details

--- OrderlyList/Employees.cs
+++ OrderlyList/Employees.cs
@@ -6,22 +6,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace OrderlyList {
+
     internal class Employee {
-        public int Quantity { get; set; }
+
         public int Id { get; set; }
         public string Nome { get; set; }
 
-        public Employee(int Quantity, int Id, string Nome) {
-            this.Quantity = Quantity;
+        public Employee(int Id, string Nome) {
+
             this.Id = Id;
             this.Nome = Nome;
-        }
-        
-
-
-        override public string ToString() {
-
-            return $"#{Quantity + 1}- ID: {Id}, Name: {Nome}";
         }
     }
 }
--- OrderlyList/Program.cs
+++ OrderlyList/Program.cs
@@ -5,46 +5,42 @@ using System.Text.Json.Serialization;
 
 namespace OrderlyList {
 
-
     class Program {
+
         static void Main(string[] args) {
 
-            List<Employee> employees = DataReceive();
-
+            List<Employee> employees = ReceberDados();
             var employeesorder = Ordenar(employees);
             Printar(employeesorder);
-
-
         }
-        public static List<Employee> DataReceive() {
+
+        static List<Employee> ReceberDados() {
 
             Console.Write("how many employees does the company have: ");
             int Quantity = int.Parse(Console.ReadLine());
 
             var list = new List<Employee>();
 
-
             for (int i = 0; i < Quantity; i++) {
-
                 Console.Write("-----------------------------\n#{0}-Employee ID: ", (i + 1));
                 int IdReceived = int.Parse(Console.ReadLine());
+
                 Console.Write("#{0}-Employee name: ", (i + 1));
                 string NameReceived = Console.ReadLine();
-                list.Add(new Employee(Quantity, IdReceived, NameReceived));
 
+                list.Add(new Employee(IdReceived, NameReceived));
             }
+
             return list;
         }
-        public static List<Employee> Ordenar(List<Employee> employees) {
+
+        static List<Employee> Ordenar(List<Employee> employees) {
 
             var ordenados = new int[employees.Count];
 
-
             foreach (Employee cada in employees) {
                 for (int i = 0; i < ordenados.Length; i++) {
-                    if (cada.Id  <   ordenados[i]  || ordenados[i] == 0 ) {
-
-
+                    if (cada.Id < ordenados[i] || ordenados[i] == 0) {                        
                         if (ordenados[i] != 0) {
                             ordenados[i + 1] = ordenados[i];
                         }
@@ -54,23 +50,20 @@ namespace OrderlyList {
                     }
                 }
             }
+
             var listanova = new List<Employee>();
+
             foreach (int Id in ordenados) {
                 listanova.Add(employees.First(x => x.Id == Id));
             }
-
             return listanova;
-
         }
-        public static void Printar(List<Employee> employees) {
+
+        static void Printar(List<Employee> employees) {
 
             foreach (Employee cada in employees) {
-
                 Console.WriteLine(cada);
-
             }
-
-
         }
     }
 }
